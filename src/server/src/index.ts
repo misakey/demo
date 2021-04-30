04385abb93b8c5e226a0a5c957488eb17d985cb4
Antoine <antoine.vadot@misakey.com>
2021-04-30 17:48:24 +0200

    adding node_tls_reject env var

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -18,14 +18,14 @@ app.use(morgan(':method :url', { immediate: true, stream: { write: (message:stri
 app.use(morgan(process.env.ENV === 'development' ? 'dev' : 'tiny', { stream: { write: (message:string) => logger.info(message) }}));
 app.use(express.json());
 
-if (process.env.ENV === 'development') {
-    const allowCrossDomain = function(req:any, res:any, next:any) {
-        res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // allow requests from any other server
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
-    }
-    app.use(allowCrossDomain); // plumbing it in as middleware
-}
+// if (process.env.ENV === 'development') {
+//     const allowCrossDomain = function(req:any, res:any, next:any) {
+//         res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // allow requests from any other server
+//         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
+//         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+//     }
+//     app.use(allowCrossDomain); // plumbing it in as middleware
+// }
 
 // Configure routes
 registerRoutes(app);
