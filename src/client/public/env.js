b22e9eb89480189fd78a8cdc93df57c61f82a73a
= <antoine.vadot@misakey.com>
2021-05-20 10:23:18 +0200

    update favicon

--- src/client/public/env.js
+++ src/client/public/env.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-undef
 window.env = {
   API_ENDPOINT: 'http://localhost:5000',
-  MISAKEY_CLIENT_ID: '00cd5561-e294-473a-931b-11978c3d61a2',
-  MISAKEY_PRODUCER_ORG_SLUG: 'org_demo',
+  MISAKEY_CLIENT_ID: 'eb28ce9d-ba28-4e76-8aea-7d1678458d37',
+  MISAKEY_PRODUCER_ORG_SLUG: 'demo_producer',
+  MISAKEY_REDIRECT_URI: 'http://localhost:5000/user/callback',
 }
