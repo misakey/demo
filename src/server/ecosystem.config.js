bf5b4eb4ecb71b738000375e447d35c2a626e0c1
= <antoine.vadot@misakey.com>
2021-05-26 11:41:58 +0200

    remove cluster mode

--- src/server/ecosystem.config.js
+++ src/server/ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = [
   {
     name: "demo",
     script: "dist/index.js",
-    instances: 2,
-    exec_mode: 'cluster',
+    // instances: 2,
+    // exec_mode: 'cluster',
     env: {
       "NODE_ENV": "production",
       "MISAKEY_SDK_BASE_TARGET_DOMAIN": process.env.MISAKEY_SDK_BASE_TARGET_DOMAIN,
